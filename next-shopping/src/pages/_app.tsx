120928db1910385b423a60e52ad40ba9156fc05b
takuya-inoue <takuya.inoue@waku-2.com>
2023-02-23 19:06:32 +0900

    reset.css導入、globalスタイル適用テスト

--- next-shopping/src/pages/_app.tsx
+++ next-shopping/src/pages/_app.tsx
@@ -1,5 +1,18 @@
 import type { AppProps } from 'next/app';
+import { Global, css } from '@emotion/react';
+import 'modern-css-reset/dist/reset.min.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const global = css`
+    body {
+      background: #333;
+    }
+  `;
+
+  return (
+    <>
+      <Global styles={global} />
+      <Component {...pageProps} />
+    </>
+  );
 }
